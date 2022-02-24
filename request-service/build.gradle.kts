aea22c4ff0162041474470d083c949abd72986b5
choi seoin <inseo@uupfi.com>
2022-02-24 16:10:47 +0900

    add : retrofit, jackson converter dependency

--- request-service/build.gradle.kts
+++ request-service/build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
